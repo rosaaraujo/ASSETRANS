aed9f10f8e380241150060f868894924d0e5eb05
Rosa Araújo <rav@etsa.es>
2026-05-06 14:00:28 +0200

    Fase 2

---
+++ src/main/java/com/araujo/assetrans/AssetransApplication.java
@@ -0,0 +1,11 @@
+package com.araujo.assetrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetransApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AssetransApplication.class, args);
+  }
+}
